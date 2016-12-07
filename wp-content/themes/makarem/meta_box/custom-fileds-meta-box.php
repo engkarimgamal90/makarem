eb5f8ec8cd43fe094aa6b47b767e2f093a62d873
Karim Gamal <eng.karimgamal90@gmail.com>
2016-12-07 04:53:26 +0200

    create project structure

--- wp-content/themes/makarem/meta_box/custom-fileds-meta-box.php
+++ wp-content/themes/makarem/meta_box/custom-fileds-meta-box.php
@@ -1,32 +1,218 @@
 <?php
 
-// Page MetaBox
-add_filter('rwmb_meta_boxes', 'cards_assign_meta_boxes');
-function cards_assign_meta_boxes($meta_boxes) {
+// All MetaBox
+add_filter('rwmb_meta_boxes', 'makarem_custom_fields');
+function makarem_custom_fields($meta_boxes) {
 
-    $prefix = 'cards_';
-    /**
-     * General fields
-     */
+    $prefix = 'hotel_';
     $meta_boxes[] = array(
-        'id' => 'cards_pos1',
-        'title' => __('Cards Position 1', 'your-prefix'),
-        'post_types' => array('page'),
+        'id' => 'hotels_data',
+        'title' => __('Hotel data', 'mk_admin'),
+        'post_types' => array('hotel'),
         'context' => 'normal',
         'priority' => 'high',
         'autosave' => true,
         'fields' => array(
             array(
-                'name' => __('Cards for position 1', 'your-prefix'),
-                'id' => $prefix . 'pos1',
-                'desc' => __('Cards to be added to position 1', 'your-prefix'),
+                'name' => __('Banner image', 'mk_admin'),
+                'id' => $prefix . 'banner',
+                'type' => 'plupload_image',
+                'max_file_uploads' => 1,
+            ),
+            array(
+                'name' => __('Logo image', 'mk_admin'),
+                'id' => $prefix . 'logo',
+                'type' => 'plupload_image',
+                'max_file_uploads' => 1,
+            ),
+            array(
+                'name' => __('Gallery(add/select multiple images)', 'mk_admin'),
+                'id' => $prefix . 'gallery',
+                'type' => 'plupload_image',
+                'max_file_uploads' => 100,
+            ),
+            array(
+                'name' => __('Check in time', 'mk_admin'),
+                'id' => $prefix . 'check_in',
+                'type' => 'time',
+            ),
+            array(
+                'name' => __('Check out time', 'mk_admin'),
+                'id' => $prefix . 'check_out',
+                'type' => 'time',
+            ),
+            array(
+                'name' => __('Address', 'mk_admin'),
+                'id' => $prefix . 'address',
                 'type' => 'text',
-                'multiple' => true,
+            ),
+            array(
+                'name' => __('Latitude', 'mk_admin'),
+                'id' => $prefix . 'latitude',
+                'type' => 'text',
+            ),
+            array(
+                'name' => __('Longitude', 'mk_admin'),
+                'id' => $prefix . 'longitude',
+                'type' => 'text',
+            ),
+            array(
+                'name' => __('Phone', 'mk_admin'),
+                'id' => $prefix . 'phone',
+                'type' => 'text',
+            ),
+            array(
+                'name' => __('Fax', 'mk_admin'),
+                'id' => $prefix . 'fax',
+                'type' => 'text',
+            ),
+            array(
+                'name' => __('Email', 'mk_admin'),
+                'id' => $prefix . 'email',
+                'type' => 'email',
+            ),
+            array(
+                'name' => __('City', 'mk_admin'),
+                'id' => $prefix . 'city',
+                'type'        => 'post',
+                'post_type'   => 'region',
+                'field_type'  => 'select_advanced',
+                'placeholder' => esc_html__( 'Select a city', 'mk_admin' ),
+                'query_args'  => array(
+                    'post_status'    => 'publish',
+                    'posts_per_page' => -1,
+                    'post_parent__not_in' => array(0),
+                ),
             ),
         ),
         
     );
     
+    // Branches
+    $prefix = 'branch_';
+    $meta_boxes[] = array(
+        'id' => 'branches_data',
+        'title' => __('Branch data', 'mk_admin'),
+        'post_types' => array('branch'),
+        'context' => 'normal',
+        'priority' => 'high',
+        'autosave' => true,
+        'fields' => array(
+            array(
+                'name' => __('Address', 'mk_admin'),
+                'id' => $prefix . 'address',
+                'type' => 'text',
+            ),
+            array(
+                'name' => __('Latitude', 'mk_admin'),
+                'id' => $prefix . 'latitude',
+                'type' => 'text',
+            ),
+            array(
+                'name' => __('Longitude', 'mk_admin'),
+                'id' => $prefix . 'longitude',
+                'type' => 'text',
+            ),
+            
+            array(
+                'name' => __('City', 'mk_admin'),
+                'id' => $prefix . 'city',
+                'type'        => 'post',
+                'post_type'   => 'region',
+                'field_type'  => 'select_advanced',
+                'placeholder' => esc_html__( 'Select a city', 'mk_admin' ),
+                'query_args'  => array(
+                    'post_status'    => 'publish',
+                    'posts_per_page' => -1,
+                    'post_parent__not_in' => array(0),
+                ),
+            ),
+            array(
+                'name' => __('Hotel', 'mk_admin'),
+                'id' => $prefix . 'hotel',
+                'type'        => 'post',
+                'post_type'   => 'hotel',
+                'field_type'  => 'select_advanced',
+                'placeholder' => esc_html__( 'Select a hotel', 'mk_admin' ),
+                'query_args'  => array(
+                    'post_status'    => 'publish',
+                    'posts_per_page' => -1,
+                ),
+            ),
+        ),
+        
+    );
+
+    // Rooms
+    $prefix = 'room_';
+    $meta_boxes[] = array(
+        'id' => 'rooms_data',
+        'title' => __('Room data', 'mk_admin'),
+        'post_types' => array('room'),
+        'context' => 'normal',
+        'priority' => 'high',
+        'autosave' => true,
+        'fields' => array(
+            array(
+                'name' => __('Room image', 'mk_admin'),
+                'id' => $prefix . 'image',
+                'type' => 'plupload_image',
+                'max_file_uploads' => 1,
+            ),
+            array(
+                'name' => __('Number of beds', 'mk_admin'),
+                'id' => $prefix . 'num_of_beds',
+                'type' => 'number',
+                'min'  => 1,
+            ),
+            array(
+                'name' => __('Hotel', 'mk_admin'),
+                'id' => $prefix . 'hotel',
+                'type'        => 'post',
+                'post_type'   => 'hotel',
+                'field_type'  => 'select_advanced',
+                'placeholder' => esc_html__( 'Select a hotel', 'mk_admin' ),
+                'query_args'  => array(
+                    'post_status'    => 'publish',
+                    'posts_per_page' => -1,
+                ),
+            ),
+            array(
+                'name' => __('Packages', 'mk_admin'),
+                'id' => $prefix . 'package',
+                'type'        => 'post',
+                'post_type'   => 'package',
+                'field_type'  => 'select_advanced',
+                'placeholder' => esc_html__( 'Select package(s)', 'mk_admin' ),
+                'query_args'  => array(
+                    'post_status'    => 'publish',
+                    'posts_per_page' => -1,
+                ),
+                'multiple'    => true,
+            ),
+        ),
+        
+    );
+    
+    // Packages
+    $prefix = 'package_';
+    $meta_boxes[] = array(
+        'id' => 'packages_data',
+        'title' => __('Package data', 'mk_admin'),
+        'post_types' => array('package'),
+        'context' => 'normal',
+        'priority' => 'high',
+        'autosave' => true,
+        'fields' => array(
+            array(
+                'name' => __('Average price', 'mk_admin'),
+                'id' => $prefix . 'price',
+                'type' => 'text',
+            ),
+            
+        ),
+        
+    );
     return $meta_boxes;
 }
 
--- wp-content/themes/makarem/meta_box/custom-post-types.php
+++ wp-content/themes/makarem/meta_box/custom-post-types.php
@@ -3,36 +3,155 @@
  * Custom  Post Types
  */
 
-//VENUES
-function custom_post_venues() {
+//HOTELS
+function custom_post_hotels() {
     $labels = array(
-        'name' => _x('Venues', 'post type general name'),
-        'singular_name' => _x('Venue', 'post type singular name'),
+        'name' => _x('Hotels', 'post type general name'),
+        'singular_name' => _x('Hotel', 'post type singular name'),
         'add_new' => _x('Add New', 'venue'),
-        'add_new_item' => __('Add New Venue'),
-        'edit_item' => __('Edit Venue'),
-        'new_item' => __('New Venue'),
-        'all_items' => __('All Venues'),
-        'view_item' => __('View Venue'),
-        'search_items' => __('Search Venue'),
-        'not_found' => __('No Venues found'),
-        'not_found_in_trash' => __('No Venues found in the Trash'),
-        'taxonomies' => array('main_categories','cities'),
+        'add_new_item' => __('Add New Hotel'),
+        'edit_item' => __('Edit Hotel'),
+        'new_item' => __('New Hotel'),
+        'all_items' => __('All Hotel'),
+        'view_item' => __('View Hotel'),
+        'search_items' => __('Search Hotel'),
+        'not_found' => __('No Hotels found'),
+        'not_found_in_trash' => __('No Hotels found in the Trash'),
         'parent_item_colon' => '',
-        'menu_name' => 'Venues'
+        'menu_name' => 'Hotels'
     );
     $args = array(
         'labels' => $labels,
-        'description' => 'Holds our products and product specific data',
+        'description' => 'Holds data',
         'public' => true,
         'menu_position' => 5,
-        'supports' => array('title', 'thumbnail', 'excerpt', 'comments'),
+        'supports' => array('title', 'editor'),
         'has_archive' => true,
-        'taxonomies' => array('main_categories','cities'),
-        'hierarchical' => true,
+        'hierarchical' => false,
     );
-    register_post_type('venue', $args);
+    register_post_type('hotel', $args);
 }
+add_action('init', 'custom_post_hotels');
+//----------------------------------------------------------------------------------
+//ROOMS
+function custom_post_rooms() {
+    $labels = array(
+        'name' => _x('Rooms', 'post type general name'),
+        'singular_name' => _x('Room', 'post type singular name'),
+        'add_new' => _x('Add New', 'venue'),
+        'add_new_item' => __('Add New Room'),
+        'edit_item' => __('Edit Room'),
+        'new_item' => __('New Room'),
+        'all_items' => __('All Room'),
+        'view_item' => __('View Room'),
+        'search_items' => __('Search Room'),
+        'not_found' => __('No Rooms found'),
+        'not_found_in_trash' => __('No Rooms found in the Trash'),
+        'parent_item_colon' => '',
+        'menu_name' => 'Rooms'
+    );
+    $args = array(
+        'labels' => $labels,
+        'description' => 'Holds data',
+        'public' => true,
+        'menu_position' => 6,
+        'supports' => array('title','editor'),
+        'has_archive' => true,
+        'hierarchical' => true
+    );
+    register_post_type('room', $args);
+}
+add_action('init', 'custom_post_rooms');
 
-add_action('init', 'custom_post_venues');
+//----------------------------------------------------------------------------------
+//PACKAGES
+function custom_post_packages() {
+    $labels = array(
+        'name' => _x('Packages', 'post type general name'),
+        'singular_name' => _x('Package', 'post type singular name'),
+        'add_new' => _x('Add New', 'venue'),
+        'add_new_item' => __('Add New Package'),
+        'edit_item' => __('Edit Package'),
+        'new_item' => __('New Package'),
+        'all_items' => __('All Package'),
+        'view_item' => __('View Package'),
+        'search_items' => __('Search Package'),
+        'not_found' => __('No Packages found'),
+        'not_found_in_trash' => __('No Packages found in the Trash'),
+        'parent_item_colon' => '',
+        'menu_name' => 'Packages'
+    );
+    $args = array(
+        'labels' => $labels,
+        'description' => 'Holds data',
+        'public' => true,
+        'menu_position' => 7,
+        'supports' => array('title','editor'),
+        'has_archive' => true,
+        'hierarchical' => true
+    );
+    register_post_type('package', $args);
+}
+add_action('init', 'custom_post_packages');
+//----------------------------------------------------------------------------------
+//COUNTRIES & CITITES
+function custom_post_regions() {
+    $labels = array(
+        'name' => _x('Regions', 'post type general name'),
+        'singular_name' => _x('Region', 'post type singular name'),
+        'add_new' => _x('Add New', 'venue'),
+        'add_new_item' => __('Add New Region'),
+        'edit_item' => __('Edit Region'),
+        'new_item' => __('New Region'),
+        'all_items' => __('All Region'),
+        'view_item' => __('View Region'),
+        'search_items' => __('Search Region'),
+        'not_found' => __('No Regions found'),
+        'not_found_in_trash' => __('No Regions found in the Trash'),
+        'parent_item_colon' => '',
+        'menu_name' => 'Regions'
+    );
+    $args = array(
+        'labels' => $labels,
+        'description' => 'Holds data',
+        'public' => true,
+        'menu_position' => 8,
+        'supports' => array('title','page-attributes'),
+        'has_archive' => true,
+        'hierarchical' => true
+    );
+    register_post_type('region', $args);
+}
+add_action('init', 'custom_post_regions');
+
+//----------------------------------------------------------------------------------
+//BRANCHES
+function custom_post_branches() {
+    $labels = array(
+        'name' => _x('Branches', 'post type general name'),
+        'singular_name' => _x('Branch', 'post type singular name'),
+        'add_new' => _x('Add New', 'venue'),
+        'add_new_item' => __('Add New Branch'),
+        'edit_item' => __('Edit Branch'),
+        'new_item' => __('New Branch'),
+        'all_items' => __('All Branch'),
+        'view_item' => __('View Branch'),
+        'search_items' => __('Search Branch'),
+        'not_found' => __('No Branches found'),
+        'not_found_in_trash' => __('No Branches found in the Trash'),
+        'parent_item_colon' => '',
+        'menu_name' => 'Branches'
+    );
+    $args = array(
+        'labels' => $labels,
+        'description' => 'Holds data',
+        'public' => true,
+        'menu_position' => 9,
+        'supports' => array('title'),
+        'has_archive' => true,
+        'hierarchical' => true
+    );
+    register_post_type('branch', $args);
+}
+add_action('init', 'custom_post_branches');
 
